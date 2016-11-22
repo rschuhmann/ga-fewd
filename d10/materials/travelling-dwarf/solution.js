6065b9dc9b6291ddf988c068b7546ba08aac4499
rschuhmann <reinhardt.schuhmann@gmail.com>
2016-11-22 18:35:53 -0500

    Week 5 HW

---
+++ d10/materials/travelling-dwarf/solution.js
@@ -0,0 +1,61 @@
+console.log("WRITE YOUR SOLUTION IN HERE!");
+var i = 1;
+
+while (i <= 4) {
+	d.move();
+	i = i + 1;	
+}
+
+d.orient("down");
+
+var i = 1;
+
+while (i <= 16) {
+	d.move();
+	i = i+1;
+}
+
+d.orient("right");
+
+var i = 1;
+
+while (i <= 6) {
+	d.move();
+	i = i+1;
+}
+
+d.orient("up");
+
+var i = 1;
+
+while (i <= 11) {
+	d.move();
+	i = i+1;
+}
+
+d.orient("right");
+
+var i = 1;
+
+while (i <= 6) {
+	d.move();
+	i = i+1;
+} 
+
+d.orient("down");
+
+var i = 1;
+
+while (i <= 9) {
+	d.move();
+	i = i+1;
+}
+
+d.orient("right");
+
+var i = 1;
+
+while (i <= 2) {
+	d.move();
+	i = i+1;
+}
